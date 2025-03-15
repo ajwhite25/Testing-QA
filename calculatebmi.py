db02aa8304c9b9419f97b407b5865e08c9569be4
Amiya W <Whiteamiya17@gmail.com>
2025-03-15 16:45:51 -0500

    initial test driven development

---
+++ calculatebmi.py
@@ -0,0 +1,26 @@
+import math
+
+def convert_weight_to_kg(weight):
+    pass
+
+def convert_height_to_meters(height_ft, height_in):
+    pass
+
+def calculate_bmi(weight, height_ft, height_in):
+    pass
+
+def bmi_category(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    elif bmi >= 18.5 and bmi <= 24.9:
+        return "Normal weight"
+    elif bmi >= 25 and bmi <= 29.9:
+        return "Overweight"
+    else:
+        return "Obese"
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ test_calculatebmi.py
@@ -0,0 +1,24 @@
+#bmi.py
+from unittest.mock import patch
+import pytest
+from calculatebmi import convert_height_to_meters, convert_weight_to_kg, calculate_bmi, bmi_category
+
+def test_convert_weight_to_kg(weight):
+    pass
+
+def test_convert_height_to_meters(height_ft, height_in):
+    pass
+
+def test_calculate_bmi(weight, height_ft, height_in):
+    pass
+
+@pytest.mark.parametrize("bmi, category",[
+(18.0, "Underweight"),
+(18.4, "Underweight"),
+(18.5, "Normal weight"),
+(20, "Normal weight"),
+(25, "Overweight"),
+(30, "Obese"),
+])
+def test_bmi_category(bmi, category):
+    assert bmi_category(bmi) == category
