0fac29a5d7889ac52dba3260e2303e7a64127480
Amiya W <Whiteamiya17@gmail.com>
2025-03-18 08:51:46 -0500

    initial commit/ completed

--- calculatebmi.py
+++ calculatebmi.py
@@ -1,14 +1,24 @@
 import math
 
 def convert_weight_to_kg(weight):
+    if weight <=0:
+        raise ValueError("Weight must be greater than 0.")
     return weight * 0.45
 
 def convert_height_to_meters(height_ft, height_in):
+    if height_in >= 12:
+        raise ValueError("Inches must fall between 0 and 11.")
     total_in = (height_ft * 12) + height_in
     return total_in * 0.025
 
 def calculate_bmi(weight, height_ft, height_in):
-    pass
+    weight_kg = convert_weight_to_kg(weight)
+    height_m = convert_height_to_meters(height_ft, height_in)
+
+    if height_m == 0:
+        raise ValueError("Height cannot be zero")
+    
+    return weight_kg / (height_m ** 2)
 
 def bmi_category(bmi):
     if bmi < 18.5:
@@ -21,7 +31,25 @@ def bmi_category(bmi):
         return "Obese"
 
 def main():
-    pass
+    print("\nWelcome to BMI Calculator. Let's Begin!\n")
+
+    try:
+        height = input("Enter a height(ft' in): ").split("'")
+        weight = float(input("Enter a weight(Ibs): "))
+    
+        if len(height) != 2:
+            print("Invalid height format. Use format: 5'10")
+
+        feet = int(height[0].strip())
+        inches = int(height[1].strip())
+        bmi = round(calculate_bmi(weight, feet, inches),1)
+        category = bmi_category(bmi)
+        print(f"The BMI for a person who is {feet}'{inches} and weighs {weight} Ibs is {bmi}.")
+        print(f"With this BMI, they would be categorized as {category}.\n")
+
+    except ValueError as e:
+        print(f"Invalid input: {e}")
+
 
 if __name__ == "__main__":
     main()
--- test_calculatebmi.py
+++ test_calculatebmi.py
@@ -1,16 +1,53 @@
-#bmi.py
+#calculatebmi.py
+import unittest
 from unittest.mock import patch
 import pytest
-from calculatebmi import convert_height_to_meters, convert_weight_to_kg, calculate_bmi, bmi_category
+from calculatebmi import convert_height_to_meters, convert_weight_to_kg, calculate_bmi, bmi_category, main
 
+@pytest.mark.parametrize( "weight, conversion", [
+    (1, 0.45),
+    (1.1, 0.495),
+    (125, 56.25),
+    (1000, 450),
+])
 def test_convert_weight_to_kg(weight, conversion):
-    assert convert_weight_to_kg (weight) == conversion
+    assert convert_weight_to_kg (weight) ==  pytest.approx(conversion, rel=1e-3) # Pytest documentation
 
-def test_convert_height_to_meters(height_ft, height_in, conversion):
-    assert convert_height_to_meters(height_ft, height_in) == conversion
+# ensure out of bounds test cases are caught and fail!
+@pytest.mark.parametrize("weight", [
+    0, -1, -10
+])
+def test_invalid_weight(weight):
+    with pytest.raises(ValueError):
+        convert_weight_to_kg(weight)
 
-def test_calculate_bmi(weight, height_ft, height_in):
-    pass
+@pytest.mark.parametrize( "ft, inches, conversion", [
+    (0, 1, 0.025),
+    (0, 2, 0.05),
+    (5, 3, 1.575),
+    (10, 10, 3.25),
+    (10, 11, 3.275),
+])
+def test_convert_height_to_meters(ft, inches, conversion):
+    assert convert_height_to_meters(ft, inches) == pytest.approx(conversion, rel=1e-3)
+
+@pytest.mark.parametrize("ft, inches", [
+    (5,12),
+    (10, 14),
+])
+def test_invalid_height(ft, inches):
+    with pytest.raises(ValueError):
+        convert_height_to_meters(ft, inches)
+
+@pytest.mark.parametrize("weight, ft, inches, expected", [
+    (125, 5, 3, 22.7),
+    (165, 5, 8, 25.7),
+    (90, 5, 5, 15.3),
+    (200, 5, 11, 28.6),
+    (250, 5, 0, 50),
+])
+def test_calculate_bmi(weight, ft, inches, expected):
+    assert calculate_bmi(weight, ft, inches) == pytest.approx(expected, rel=1e-1)
 
 @pytest.mark.parametrize("bmi, category",[
 (18.0, "Underweight"),
